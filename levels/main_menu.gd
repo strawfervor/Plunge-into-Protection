5654c4b94e0e0f3ce2b23bcacc04f2fa86f429fe
6663666 <95314076+strawfervor@users.noreply.github.com>
2023-02-14 08:08:08 +0000

    main menu
    Added main menu node

--- levels/main_menu.gd
+++ levels/main_menu.gd
@@ -2,6 +2,7 @@ extends Control
 
 func _ready():
 	$VBoxContainer/Start.grab_focus()
+	$CreditsText.hide()
 
 func _on_start_pressed():
 	get_tree().change_scene_to_file("res://levels/classic_levels.tscn")
@@ -9,3 +10,7 @@ func _on_start_pressed():
 
 func _on_exit_pressed():
 	get_tree().quit()
+
+
+func _on_credits_pressed():
+	$CreditsText.show()
