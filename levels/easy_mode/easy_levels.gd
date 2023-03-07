f5660e8df93343ba8ce809135ce741101254a641
6663666 <95314076+strawfervor@users.noreply.github.com>
2023-03-07 21:31:50 +0000

    added easy mode
    Added easy mode to the game, some fixes to the story mode scenes, that makes it clearer

---
+++ levels/easy_mode/easy_levels.gd
@@ -0,0 +1,63 @@
+extends Node2D
+
+var level_number = 1
+var player_lives = 3
+var score = 0
+var enemies_left = 0
+var level
+var points = 0
+var level_type = 1
+var game_over = false
+
+func _ready():
+	$UI/Phase.text = "PHASE " + str(level_number)
+	$UI/Phase.show()
+	$UI/Points.text = "SCORE: " + str(points)
+	$UI/Points.show()
+	$UI/Lives.hide()
+	level = get_node("Level")
+	level.next_level_please.connect(_next_level)
+
+
+func _on_phase_show_timeout():
+	$UI/Phase.hide()
+
+func _next_level():
+	$NextLevelTimer.start()
+	level_number += 1
+	$UI/Phase.text = "PHASE " + str(level_number)
+	$UI/Phase.show()
+	$UI/PhaseShow.start()
+	
+func update_score():
+	points += 100 + (level_number * 10)
+	$UI/Points.text = "SCORE: " + str(points)
+	$UI/Points.show()
+
+func live_lost():
+	if game_over == false:
+		points -= 200
+		$UI/Points.text = "SCORE: " + str(points)
+		$UI/Points.show()
+	
+	if player_lives == 0 && game_over == false:
+		game_over = true
+		$UI/Lives.hide()
+		$UI/Points.hide()
+		level.on_game_over()
+
+
+func _on_next_level_timer_timeout():
+	var cur_level = level_number % 9
+	if cur_level == 1 or cur_level == 2:
+		level_type = 1
+	elif cur_level == 3 or cur_level == 4:
+		level_type = 2
+	elif cur_level == 5 or cur_level == 6:
+		level_type = 3
+	else:
+		level_type = 4
+	if (cur_level) > 0:
+		level.load_level((cur_level - 1), level_number - 50, level_type)
+	else:
+		level.load_level(8, level_number - 50, 4)
---
+++ levels/easy_mode/easy_levels_setter.gd
@@ -0,0 +1,96 @@
+extends Node2D
+
+var level_classic_1 = preload("res://levels/classic_level_1_blue.tscn")
+var level_classic_2 = preload("res://levels/classic_level_2_orange_short.tscn")
+var level_classic_3 = preload("res://levels/classic_level_3_gemlike_purple.tscn")
+var level_game_over = preload("res://levels/game_over.tscn")
+var level_classic_4 = preload("res://test_scene.tscn")
+var level_random_1 = preload("res://levels/level_random_1.tscn")
+var next_level
+var current_level
+var rat = preload("res://enemy_rat.tscn")
+var scorpio = preload("res://enemies/enemy_scorpio.tscn")
+var fly = preload("res://enemies/enemy_fly.tscn")
+var levels_array = [
+	[rat, rat],
+	[rat, rat, rat],
+	[scorpio, scorpio, scorpio],
+	[scorpio, scorpio, scorpio, rat],
+	[fly, fly, fly],
+	[fly, scorpio, scorpio, fly],
+	[rat, rat, fly, scorpio, fly],
+	[scorpio, scorpio, fly, fly, rat],
+	[scorpio, fly, scorpio, fly, fly, scorpio]
+]
+
+var root
+
+var level_loaded = false
+var spawner
+
+var points = 0
+
+var pow_block_status = 3
+
+signal next_level_please
+
+func _process(delta):
+	if level_loaded:
+		if spawner.level_finished:
+			on_finished_level()
+
+func _ready():
+	root = get_parent()
+	load_level(0, -50, 1)
+	stats_update()
+
+func load_level(level_number, speed_modificator, level_type):
+	match level_type:
+		1:
+			next_level = level_classic_1
+		2:
+			next_level = level_classic_2
+		3:
+			next_level = level_classic_3
+		4:
+			next_level = level_classic_4
+		"random":
+			next_level = level_random_1
+	
+	current_level = next_level.instantiate()
+	set_level(level_number, speed_modificator)
+	level_loaded = true
+	add_child(current_level)
+	current_level.get_node("Player").im_dead.connect(_player_dead)
+
+func stats_update():
+	print("stats update")
+
+func set_level(number, speed):
+	spawner = current_level.get_node("EnemySpawner")
+	spawner.enemies_random = false #true gereting random enemies, false generete enemies from list, one by one
+	spawner.max_enemies = 1
+	spawner.enemies_list = levels_array[number] #list of enemies to spawn (rat, scorpio, fly)
+	spawner.speed_modificator = speed
+	spawner.add_points.connect(_add_points)
+	current_level.get_node("POW_block").hitted_pow_block.connect(_hitted_pow_block)
+	current_level.get_node("POW_block").status = pow_block_status
+
+func on_finished_level():
+	level_loaded = false
+	remove_child(current_level)
+	next_level_please.emit()
+
+func _add_points():
+	root.update_score()
+	
+func _hitted_pow_block():
+	pow_block_status -= 1
+
+func _player_dead():
+	root.live_lost()
+
+func on_game_over():
+	remove_child(current_level)
+	current_level = level_game_over.instantiate()
+	add_child(current_level)
